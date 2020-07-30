216a3af03ec6eda5d64dcb0feeb9c4ad1a2d6422
Ted M <practice@example.com>
2020-07-29 23:57:12 -0400

    Worked on book examples

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -21,11 +21,14 @@ public class Event {
     @Email(message = "Invalid email.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
+    private EventType type;
+
+    public Event(String name, String description, String contactEmail, EventType type) {
         this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.type = type;
     }
 
     public Event() {
@@ -55,6 +58,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public EventType getType() {
+        return type;
+    }
+
+    public void setType(EventType type) {
+        this.type = type;
     }
 
     public int getId() {
