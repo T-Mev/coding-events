213ea6511807be89ac599cfd11fe1a8a76b2acd9
Ted M <practice@example.com>
2020-07-29 22:35:27 -0400

    Completed Chapter 13 exercises

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,8 +1,6 @@
 package org.launchcode.codingevents.models;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
+import javax.validation.constraints.*;
 import java.util.Objects;
 
 public class Event {
@@ -21,11 +19,29 @@ public class Event {
     @Email(message = "Invalid email.")
     private String contactEmail;
 
-    public Event(String name, String description, String contactEmail) {
+    @NotBlank(message = "Location required!")
+    private String location;
+
+    @AssertTrue(message = "Must be set to true!")
+    private Boolean register;
+
+    @NotNull(message = "Number of Attendees required!")
+    @Min(value = 1, message = "Value has to be greater than 0.")
+    private Integer attendees;
+
+    @NotBlank(message = "Attendees required!")
+    private String entryFee;
+
+
+    public Event(String name, String description, String contactEmail, String location, Boolean register, Integer attendees, String entryFee) {
         this();
         this.name = name;
         this.description = description;
         this.contactEmail = contactEmail;
+        this.location = location;
+        this.register = register;
+        this.attendees = attendees;
+        this.entryFee = entryFee;
     }
 
     public Event() {
@@ -55,6 +71,38 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public Boolean getRegister() {
+        return register;
+    }
+
+    public void setRegister(Boolean register) {
+        this.register = register;
+    }
+
+    public Integer getAttendees() {
+        return attendees;
+    }
+
+    public void setAttendees(Integer attendees) {
+        this.attendees = attendees;
+    }
+
+    public String getEntryFee() {
+        return entryFee;
+    }
+
+    public void setEntryFee(String entryFee) {
+        this.entryFee = entryFee;
     }
 
     public int getId() {
